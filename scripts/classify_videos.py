e4bf97ca0f879e331b1030a1473a989fce5bdccf
Ana Flo <anaflom@gmail.com>
2026-02-10 15:34:07 +0100

    rearange files and folders

---
+++ scripts/classify_videos.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+from managedata.handle_dataset import DataSet
+
+def main(folder_data, folder_results):
+
+    # initialize a datset object to handle the data and metadata
+    dataset = DataSet(folder_data, folder_metadata=None, folder_metadata_per_trial=folder_results)
+    
+    # clasiffy the videos for all recordings
+    dataset.clasiffy_videos()
+    
+    
+
+if __name__ == "__main__":
+
+    # base folder
+    folder_data = '/home/anaflo/MDMC/thesis/sensorium/data/'
+    # results folder
+    folder_results = '/home/anaflo/MDMC/thesis/sensorium/intermediate_results/'
+
+    try:
+        main(folder_data, folder_results)
+        print("\nVideo classification completed successfully!")
+    except Exception as e:
+        print(f"Fatal error: {e}")
---
+++ scripts/define_segments_ids.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+from managedata.videos import VideoSegment
+from managedata.videos_duplicates import (compute_dissimilarity_video_list, find_equal_sets_scipy, generate_new_id)
+from managedata.handle_dataset import DataSet
+
+def main(folder_data, folder_meta):
+
+    # the labels to check
+    labels = ['NaturalImages','GaussianDot','Gabor','PinkNoise','RandomDots']
+
+    # set the limit to decide that two segments are the same or different
+    limit_dissimilarity = 20
+
+    # initialize a datset object to handle the data and metadata
+    dataset = DataSet(folder_data, folder_metadata=folder_meta)
+
+    # define segments IDs
+    dataset.define_segments_id(labels, limit_dissimilarity=limit_dissimilarity)
+
+    # add the information about segments IDs to the videos metadata
+    dataset.add_segments_id_to_video_metadata()
+
+
+if __name__ == "__main__":
+
+    # path to the folder with the data as downloaded
+    folder_data = '/home/anaflo/MDMC/thesis/sensorium/data/'
+    # path to the metadata folder
+    folder_meta = '/home/anaflo/MDMC/thesis/sensorium/metadata/'
+
+
+    try:
+        main(folder_data, folder_meta)
+        print("\nSegment ID definition completed successfully!")
+    except Exception as e:
+        print(f"Fatal error: {e}")
